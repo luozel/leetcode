572a9ba20f93ee0b038861205e1ebf15a7b78db8
jue2022 <984373402@qq.com>
2021-03-04 18:05:02 +0800

    3.4

---
+++ src/LeetCode13/Solution.java
@@ -0,0 +1,71 @@
+package LeetCode13;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution1 solution1 = new Solution1();
+        solution1.romanToInt1("MCMXCIV");
+    }
+}
+class Solution1{
+    public int romanToInt(String s) {
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num  = getValue(s.charAt(i));
+            if(preNum<num){
+                sum-=preNum;
+            }else {
+                sum+=preNum;
+            }
+            preNum = num;
+        }
+        sum+=preNum;
+        return sum;
+    }
+
+    private int getValue(char charAt) {
+        switch (charAt){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    public int romanToInt1(String s) {
+        s = s.replace("IV","a");
+        s = s.replace("IX","b");
+        s = s.replace("XL","c");
+        s = s.replace("XC","d");
+        s = s.replace("CD","e");
+        s = s.replace("CM","f");
+
+        int result = 0;
+        for (int i=0; i<s.length(); i++) {
+            result += which(s.charAt(i));
+        }
+        return result;
+    }
+
+    public int which(char ch) {
+        switch(ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            case 'a': return 4;
+            case 'b': return 9;
+            case 'c': return 40;
+            case 'd': return 90;
+            case 'e': return 400;
+            case 'f': return 900;
+        }
+        return 0;
+    }
+}
---
+++ src/LeetCode354/Solution.java
@@ -0,0 +1,140 @@
+package LeetCode354;
+
+import NoTest.Util.IntegerUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[][] envelopes = new int[][]{{5, 4}, {6, 6}, {6, 4}, {2, 3}};
+        Solution1 solution1 = new Solution1();
+        int i = solution1.maxEnvelopes(envelopes);
+        Solution2 solution2 = new Solution2();
+        System.out.println(solution2.maxEnvelopes(envelopes));
+    }
+}
+
+class Solution1 {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] e1, int[] e2) {
+                if (e1[0] != e2[0]) {
+                    return e1[0] - e2[0];
+                } else {
+                    return e2[1] - e1[1];
+                }
+            }
+        });
+
+        int[] f = new int[n];
+        Arrays.fill(f, 1);
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (envelopes[j][1] < envelopes[i][1]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            ans = Math.max(ans, f[i]);
+        }
+        return ans;
+    }
+}
+
+class Solution2 {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] e1, int[] e2) {
+                if (e1[0] != e2[0]) {
+                    return e1[0] - e2[0];
+                } else {
+                    return e2[1] - e1[1];
+                }
+            }
+        });
+
+        List<Integer> f = new ArrayList<Integer>();
+        f.add(envelopes[0][1]);
+        for (int i = 1; i < n; ++i) {
+            int num = envelopes[i][1];
+            if (num > f.get(f.size() - 1)) {
+                f.add(num);
+            } else {
+                int index = binarySearch(f, num);
+                f.set(index, num);
+            }
+        }
+        return f.size();
+    }
+
+    public int binarySearch(List<Integer> f, int target) {
+        int low = 0, high = f.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (f.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
+
+class Solution3 {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+        int n = envelopes.length;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(envelopes[0][1]);
+        for (int i = 1; i < n; i++) {
+            int num = envelopes[i][1];
+            if (num > list.get(list.size() - 1)) {
+                list.add(num);
+            } else {
+                int index = binarySearch(list, num);
+                list.set(index, num);
+            }
+        }
+        return list.size();
+    }
+
+    private int binarySearch(ArrayList<Integer> list, int num) {
+        int low = 0, high = list.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (list.get(mid) < num) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+}
